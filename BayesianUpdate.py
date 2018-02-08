5425770c8d11649b66468c6f83475d68135affda
Taha Dogan Gunes <tdgunes@gmail.com>
2018-02-08 13:54:43 +0000

    edit BayesianUpdate

--- BayesianUpdate.py
+++ BayesianUpdate.py
@@ -8,10 +8,10 @@ from scipy.stats import norm
 
 fig, ax = plt.subplots(1, 1)
 
-mean = -1.0
-variance = 0.2
+mean = 3.5
+variance = 0.1
 
-x = np.linspace(-2.5, 10.0, 1000)
+x = np.linspace(0.0, 10.0, 1000)
 
 ground_truth = norm(loc=5, scale=math.sqrt(1.0))
 prior = norm(loc=mean, scale=math.sqrt(variance))
@@ -26,20 +26,21 @@ def calculate_posterior(prior_mean, prior_variance, observations, known_variance
     return posterior_mean, posterior_variance
 
 
-for n in range(30):
-
-    sample_size = n
-    samples = ground_truth.rvs(size=sample_size)
-
+sample_size = 100
+sample_list = []
+for n in range(sample_size):
+    sample = ground_truth.rvs(size=1)
+    sample_list.append(sample)
+    samples = np.array(sample_list)
     p_mean, p_variance = calculate_posterior(prior.mean(), prior.var(), samples, ground_truth.var())
     posterior_est = norm(loc=p_mean, scale=math.sqrt(p_variance))
-    if n == 29:
+    if n == sample_size-1:
         ax.hist(samples, normed=True, histtype='stepfilled', alpha=0.2, label="random draws")
-        ax.plot(x, posterior_est.pdf(x), 'y', lw=2, alpha=n / 45.0,
+        ax.plot(x, posterior_est.pdf(x), 'y', lw=2, alpha=n / sample_size,
                 label="posterior N(%.2f, %.2f)" % (posterior_est.mean(),
                                                    posterior_est.var()))
     else:
-        ax.plot(x, posterior_est.pdf(x), 'y', lw=2, alpha=n / 45.0)
+        ax.plot(x, posterior_est.pdf(x), 'y', lw=2, alpha=n / sample_size)
 
 print("Stats:")
 print(prior.stats(moments="mv"))
