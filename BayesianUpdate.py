f70cd2afb162530867d5af18c45167e007859c52
Adrian Sieber <mail@adriansieber.com>
2018-02-21 23:07:44 +0000

    Fix code style

--- BayesianUpdate.py
+++ BayesianUpdate.py
@@ -18,10 +18,13 @@ prior = norm(loc=mean, scale=math.sqrt(variance))
 
 
 def calculate_posterior(prior_mean, prior_variance, observations, known_variance):
-    posterior_mean = math.pow((1.0 / prior_variance) + (observations.size / known_variance), -1)
-    posterior_mean = posterior_mean * ((prior_mean / prior_variance) + (np.sum(observations) / known_variance))
+    posterior_mean = math.pow(
+        (1.0 / prior_variance) + (observations.size / known_variance), -1)
+    posterior_mean = posterior_mean * \
+        ((prior_mean / prior_variance) + (np.sum(observations) / known_variance))
 
-    posterior_variance = (known_variance * prior_variance) / (known_variance + observations.size * prior_variance)
+    posterior_variance = (known_variance * prior_variance) / \
+        (known_variance + observations.size * prior_variance)
 
     return posterior_mean, posterior_variance
 
@@ -32,10 +35,12 @@ for n in range(sample_size):
     sample = ground_truth.rvs(size=1)
     sample_list.append(sample)
     samples = np.array(sample_list)
-    p_mean, p_variance = calculate_posterior(prior.mean(), prior.var(), samples, ground_truth.var())
+    p_mean, p_variance = calculate_posterior(
+        prior.mean(), prior.var(), samples, ground_truth.var())
     posterior_est = norm(loc=p_mean, scale=math.sqrt(p_variance))
     if n == sample_size-1:
-        ax.hist(samples, normed=True, histtype='stepfilled', alpha=0.2, label="random draws")
+        ax.hist(samples, normed=True, histtype='stepfilled',
+                alpha=0.2, label="random draws")
         ax.plot(x, posterior_est.pdf(x), 'y', lw=2, alpha=n / sample_size,
                 label="posterior N(%.2f, %.2f)" % (posterior_est.mean(),
                                                    posterior_est.var()))
@@ -45,7 +50,8 @@ for n in range(sample_size):
 print("Stats:")
 print(prior.stats(moments="mv"))
 
-ax.plot(x, prior.pdf(x), 'r-', lw=2, label="prior N(%.1f, %.1f)" % (prior.mean(), prior.var()))
+ax.plot(x, prior.pdf(x), 'r-', lw=2, label="prior N(%.1f, %.1f)" %
+        (prior.mean(), prior.var()))
 ax.plot(x, ground_truth.pdf(x), 'g-', lw=2,
         label="ground truth N(%.1f, %.1f)" % (ground_truth.mean(), ground_truth.var()))
 
